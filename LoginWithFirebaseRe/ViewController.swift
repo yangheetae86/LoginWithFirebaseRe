5425093e4373f3d37867b68880222b8f515dccf3
yang <heroparkjisung2gmail.com>
2020-04-09 16:56:27 +0900

    add Auth, Fire 추가store전

--- LoginWithFirebaseRe/ViewController.swift
+++ LoginWithFirebaseRe/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var usernameTextField: UITextField!
+    @IBAction func tappedRegisterButton(_ sender: Any) {
+        print("asd")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
